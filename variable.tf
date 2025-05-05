45c8632a1e31ba579be1b04a077819b150feb958
hruthingali <hruthingd123@gmail.com>
2025-05-05 15:06:02 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,14 @@
+# terraform-modules/ec2-instance/variables.tf
+variable "ami_id" {
+  description = "The AMI ID to use for the instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of the instance"
+  type        = string
+}
+variable "subnet_id" {
+  type=string
+
+}
