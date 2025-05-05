28c003e521ea28b2587ae9c8c8d8205b191e338f
hruthingali <hruthingd123@gmail.com>
2025-05-05 15:37:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # terraform-modules/ec2-instance/main.tf
 resource "aws_instance" "example" {
   ami           = var.ami_id
-  instance_type = var.instance_type
+  instance_type = var.instancetype
   subnet_id = var.subnet-id
 }
